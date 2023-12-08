143b604fef92288ba5ba679376a88cc0267d29c8
ShubhamGhosh1993 <shubham.ghoshofficial@gmail.com>
2023-12-08 18:28:00 +0530

    quering graph ql

---
+++ fakedb.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    id: "12132325cfdsf",
+    firstname: "Shubham",
+    lastname: "Ghosh",
+    email: "shubhamghosh@gmail.com",
+    password: "12345",
+  },
+  {
+    id: "1254545252cddsacfasd",
+    firstname: "Sayan",
+    lastname: "Das",
+    email: "sayandas@gmail.com",
+    password: "12346",
+  },
+];
+
+export const quotes = [
+  {
+    quote: "Hi there",
+    created_by: "1254545252cddsacfasd",
+  },
+  {
+    quote: "Hi there 2",
+    created_by: "12132325cfdsf",
+  },
+  {
+    quote: "Hi there another quotes",
+    created_by: "1254545252cddsacfasd",
+  },
+];
--- server.js
+++ server.js
@@ -1,29 +1,54 @@
-import {ApolloServer,gql} from "apollo-server"
-import {ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core"
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { users, quotes } from "./fakedb.js";
 // Query Schema
 const typeDefs = gql`
-type Query{
-    greet: String,
+  type Query {
+    greet: String
     users_count: Int
+    users: [Users]
+    iquote(created_by: ID!): [Quotes]
+    quotes: [Quotes]
+    user(id: ID!): Users
+  }
 
-}
-`
+  type Users {
+    id: ID!
+    firstname: String
+    lastname: String
+    email: String
+    password: String
+    quotes: [Quotes]
+  }
+
+  type Quotes {
+    quote: String
+    created_by: ID!
+  }
+`;
 // Resolvers
 const resolvers = {
-    Query:{
-        greet: ()=>"Hello World!",
-        users_count: ()=>56
-    }
-}
+  Query: {
+    greet: () => "Hello World!",
+    users_count: () => 56,
+    user: (_, args) => users.find((user) => user.id == args.id),
+    users: () => users,
+    quotes: () => quotes,
+    iquote: (_, args) =>
+      quotes.filter((quote) => quote.created_by == args.created_by),
+    //   quotes.filter((quote) => {console.log(quote)}),
+  },
+  Users: {
+    quotes: (user) => quotes.filter((q) => q.created_by == user.id),
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins:[
-        ApolloServerPluginLandingPageGraphQLPlayground()
-    ]
-})
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
-server.listen(4000).then(({url})=>{
-    console.log(`Server is on ${url}`)
+server.listen(4000).then(({ url }) => {
+  console.log(`Server is on ${url}`);
 });
